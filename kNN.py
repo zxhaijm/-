f367701cdb792381f87317d9384ade14f05934a5
zxhaijm <34505110+zxhaijm@users.noreply.github.com>
2022-01-18 18:00:41 +0800

    Create kNN.py

---
+++ kNN.py
@@ -0,0 +1,13 @@
+def kNN(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistance = sqDiffMat.sum(axis=1)
+    distances = sqDistance**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
